0bbd79e1ff389ceab1636838e824bf9b4634abda
unknown <ehdnjsdl0929@gmail.com>
2017-07-13 19:14:01 +0900

    1. 말풍선 추가 2. 코인터널 텍스트 추가 3. 게임 시작시 진동효과 수정

--- src/app.js
+++ src/app.js
@@ -1,30 +1,51 @@
 'use strict';
-var game = new Phaser.Game(720, 1230, Phaser.CANVAS, '',
-{
-  preload: preload,
-  create: create,
-  update: update
-}, false, false);
-
 var reel = {};
 var coinGroup = {};
 var slotBar = {};
 var successLine = {};
 var reg = {};
 var quake = {};
+var talk = {};
+var modal = {};
+var itemText = {};
+
+var slotBody = {};
 var money = {};
+var headerCoin = {};
+var credit = {};
+var bodyGroup = {};
+var coinTunnel = {};
+
+
+var gameWidthX = CONFIG.getConfig('GAME_WIDTH');
+var gameHeightX = CONFIG.getConfig('GAME_HEIGHT');
 
 var playCount = CONFIG.getConfig('TOTAL_PLAY_COUNT');
-var coinTunnelX = CONFIG.getConfig('COIN_TUNNEL_X');
 var coinTunnelY = CONFIG.getConfig('COIN_TUNNEL_Y');
+var reelPositionY = CONFIG.getConfig('REEL_POSITION_Y');
 
-var modal = {};
+var coinGroupPositionX =  CONFIG.getConfig('COIN_GROUP_POSITION_X');
+var reelFirstPostionX = CONFIG.getConfig('IMAGE_POSITION_X');
+
+var headerPositionY = CONFIG.getConfig('HEADER_POSITION_X');
+var headerPositionX = CONFIG.getConfig('HEADER_POSITION_Y');
+var talkPositionX = CONFIG.getConfig('TALK_POSITION_X');
+var talkPositionY = CONFIG.getConfig('TALK_POSITION_Y');
 
 var isGameover = false;
 var isGetProduct = false;
 
+var game = new Phaser.Game(gameWidthX, gameHeightX, Phaser.CANVAS, '',
+{
+  preload: preload,
+  create: create,
+  update: update
+}, false, false);
+
 function preload() {
-  game.load.image('background', 'assets/bg_1.png');
+  game.load.image('background', 'assets/bg_select.png');
+  game.load.image('slot-body', 'assets/SLOT_EMPTY_bg.png');
+  //game.load.image('background', 'assets/bg.png');
   game.load.image('credit', 'assets/credit.png');
   game.load.image('money-15', 'assets/15000.png');
   game.load.image('money-10', 'assets/10000.png');
@@ -38,6 +59,11 @@ function preload() {
   game.load.image('seven', 'assets/item3.jpg');
   game.load.image('queen', 'assets/item4.jpg');
   game.load.image('success-line', 'assets/line.png');
+  game.load.image('talk', 'assets/talk.png');
+  game.load.image('item_text_1', 'assets/item1_text.png');
+  game.load.image('item_text_2', 'assets/item2_text.png');
+  game.load.image('item_text_3', 'assets/item3_text.png');
+  game.load.image('item_text_4', 'assets/item4_text.png');
 
   game.load.image('rays', 'assets/rays.png');
   game.load.image('gameover-modal', 'assets/modal.png');
@@ -74,25 +100,31 @@ function create() {
   game.stage.disableVisibilityChange = true;
 
   var items = CONFIG.getConfig('ITEMS');
-  
+
   reel = new Reel({
     'game': game,
     'items': items
   });
 
   game.add.sprite(0, 0, 'background');
-  var credit = game.add.sprite(625, 90, 'credit');
+
+  bodyGroup = game.add.group();
+
+  slotBody = game.add.sprite(0, 0, 'slot-body');
+  bodyGroup.add(slotBody);
+  credit = game.add.sprite(headerPositionX + 190, headerPositionY, 'credit');
   credit.anchor.set(0.5, 0.5);
-  money = game.add.sprite(435, 90, 'money-' + (playCount * 5).toString());
+  bodyGroup.add(credit);
+  money = game.add.sprite(headerPositionX, headerPositionY, 'money-' + (playCount * 5).toString());
   money.anchor.set(0.5, 0.5);
-
-
-  game.add.sprite(coinTunnelX, coinTunnelY, 'coin-tunnel');
-
-  var headerCoin = game.add.sprite(537, 90, 'header-coin');
+  bodyGroup.add(money);
+  headerCoin = game.add.sprite(headerPositionX + 102, headerPositionY, 'header-coin');
   headerCoin.anchor.set(0.5, 0.5);
+  bodyGroup.add(headerCoin);
+
+  itemText = new ItemText({ game: game });
+  coinTunnel = game.add.sprite(0, coinTunnelY, 'coin-tunnel');
   //mummy.scale.set(4);
-  headerCoin.smoothed = false;
   var anim = headerCoin.animations.add('walk');
   anim.play(5, true);
 
@@ -107,10 +139,18 @@ function create() {
 
   game.world.camera.position.set(0);
 
-  successLine = game.add.sprite(70, 348, 'success-line');
+  successLine = game.add.sprite(reelFirstPostionX + 15, reelPositionY + 113, 'success-line');
   successLine.alpha = 0;
 
   modal = new Modal({game: game});
+
+  talk = game.add.sprite(talkPositionX, talkPositionY, 'talk');
+
+  var tween = game.add.tween(talk).to( { y:  talk.y - 10 }, 300, Phaser.Easing.Linear.None, false, 0, -1, true);
+  tween.interpolation(function(v, k){
+    return Phaser.Math.bezierInterpolation(v, k);
+  });
+  tween.start();
 
   game.time.events.add(Phaser.Timer.SECOND * 5, showCloseButton, this);
 
@@ -122,20 +162,19 @@ function create() {
 function createCoins() {
   for (var y = 0; y < 3; y++) {
     for (var x = 0; x < 7; x++) {
-      var coin = coinGroup.create(98 + x * 75, 950, 'coin');
+      var coin = coinGroup.create(coinGroupPositionX + x * 77, coinTunnelY + 62, 'coin');
       coin.alpha = 0;
       coin.name = 'alien' + x.toString() + y.toString();
-      coin.body.velocity.y = 200 + Math.random() * 200;
       coin.checkWorldBounds = true;
       coin.events.onOutOfBounds.add(
         function (coin) {
           coin.alpha = 0;
-          coin.reset(coin.x, 950);
-          game.add.tween(coin).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0);
-          coin.body.velocity.y = 100 + Math.random() * 200;
+          coin.reset(coin.x, coinTunnelY + 62);
+          game.add.tween(coin).to( { alpha: 1 }, 800, Phaser.Easing.Linear.None, true, 0, 0);
+          coin.body.velocity.y = 100 + Math.random() * 100;
         }, this);
-      coin.body.velocity.y = 100 + Math.random() * 200;
-      game.add.tween(coin).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0);
+      coin.body.velocity.y = 100 + Math.random() * 100;
+      game.add.tween(coin).to( { alpha: 1 }, 800, Phaser.Easing.Linear.None, true, 0, 0);
     }
   }
 }
@@ -153,7 +192,9 @@ function showCloseButton() {
   closeButton.anchor.set(0.5, 0.5);
   closeButton.events.onInputDown.add(
     function(){
-      game.destroy();
+      if(confirm('게임을 종료하고 창을 닫습니다.\n계속하시겠습니까?')) {
+        window.close();
+      }
     }, this);
 }
 
@@ -168,29 +209,38 @@ function update(){
     slotBar.setDragEnable(true);
     var selectedImages = reel.getSelected();
     var isGetProduct = checkSlotPoint(selectedImages);
+
+    //  당첨 되었을 경우
     if(isGetProduct) {
-      //  모달 생성 할 때 닫기 콜백으로 종료 모달 띄우기 구문 추가
       this.game.time.events.add(Phaser.Timer.SECOND * 2.1, function(){
         modal.showPresentModal(selectedImages.first);
         isGameover = true;
       }, this);
       slotBar.setDragEnable(false);
+      return;
     }
 
     //  획득 상품도 없고 플레이 가능 횟수도 없을 경우 game over
     if(!isGetProduct && playCount <= 0) {
-      modal.showGameOverModal();
+      this.game.time.events.add(Phaser.Timer.SECOND * 1, function(){
+        modal.showGameOverModal();
+      }, this);
       isGameover = true;
       slotBar.setDragEnable(false);
+      return;
     }
+
+    //대기 상태일 경우
+    showSpritesForWaiting();
   }
 }
 
 function spinReels() {
   successLine.alpha = 0;
-  coinGroup.killAll();
 
-  money.loadTexture('money-' + ((playCount - 1)*5).toString() , 0);
+  hideSpritesForPlaying();
+
+  money.loadTexture('money-' + ((playCount - 1) * 5).toString() , 0);
 
   addQuake();
   game.time.events.add(Phaser.Timer.SECOND * 2, stopReels, this);
@@ -217,27 +267,30 @@ function checkSlotPoint(selectedImages) {
   return result;
 }
 
-function addQuake() {
-  var rumbleOffset = 10;
-  var properties = {
-    x: game.camera.x - rumbleOffset
-  };
+function showSpritesForWaiting() {
+  talk.alpha = 1.0;
+  itemText.show();
+}
 
+function hideSpritesForPlaying() {
+  talk.alpha = 0.0;
+  itemText.hide();
+  coinGroup.killAll();
+}
+
+function addQuake() {
   var duration = 100;
   var ease = Phaser.Easing.Bounce.InOut;
-  var autoStart = false;
+  var autoStart = true;
   var delay = 0;
   var yoyo = true;
   var repeat = -1;
 
-  quake = game.add.tween(game.camera).to(properties, 100, ease, autoStart, delay, repeat, yoyo);
-  quake.start();
+  quake = game.add.tween(bodyGroup).to({x: bodyGroup.x - 10}, 100, ease, autoStart, delay, repeat, yoyo);
+
 }
 
 function stopQuake() {
   quake.stop(true);
-  var properties = {
-    x: game.camera.x + 10
-  };
-  game.add.tween(game.camera).to(properties, 100, Phaser.Easing.Bounce.InOut, true);
+  game.add.tween(bodyGroup).to({x: bodyGroup.x + 10}, 100, Phaser.Easing.Bounce.InOut, true);
 }
